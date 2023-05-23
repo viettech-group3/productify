c2a4e0f5da1b3bf64181b6721830fa412b5b4c66
deidaraiorek <phamhuudang24122004@gmail.com>
2023-05-23 13:21:32 -0400

    Update seed data file

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const generateToken = require("../utils/auth");
 const bcrypt = require("bcryptjs");
 
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const UserRouter = require("./routes/User.JS");
+const seedData = require("./config/seed");
+const faker = require("faker");
 
 // set up express server
 const app = express();
@@ -13,7 +15,22 @@ app.use("/api/users", UserRouter);
 
 //connect to port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
-  await connectDB();
-  console.log(`Server started on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    // Connect to MongoDB
+    await connectDB();
+
+    // Seed data
+    // await seedData();
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB or seeding data:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
