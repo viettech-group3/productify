55742c48489fcec1a7c8e38b2c4dcc880037b2e7
Jenny Quach <65835856+hoatuyet423@users.noreply.github.com>
2023-06-04 20:28:44 -0700

    implemented functions for event route

--- backend/controllers/Event.js
+++ backend/controllers/Event.js
@@ -57,6 +57,26 @@ const createEvent = async (req, res) => {
  * req: request object containing user id, event id, and new event info. For example, new description.
  * res: response object to return status 200.
  */
+const modifyEvent = async (req, res) => {
+  try {
+    const eventId = req.params.id;
+    const { updatedData } = req.body;
+
+    // check if user is the creator? -> might change schema for event
+    // anyone can change for now
+
+    const event = await Event.findByIdAndUpdate(
+      { _id: eventId },
+      { $set: updatedData },
+      { new: true },
+    );
+
+    res.status(200).json({ event: event });
+  } catch (error) {
+    console.log(`Failed to modify event: ${error}`);
+    res.status(500).json({ error: error });
+  }
+};
 
 /**
  * Controller function when a user finish an event
@@ -65,7 +85,96 @@ const createEvent = async (req, res) => {
  * req: request object containing user id and event id.
  * res: response object to return status 200.
  */
+/**
+ * NOTES:
+ * - might implement time caped. Rigth now, expected that start and end time is in the same day
+ * - anyone can click finish for now
+ */
+
+const finishEvent = async (req, res) => {
+  try {
+    const eventId = req.params.id;
+
+    // the point will only be distributed if user finish the event before the end time
+
+    const event = await Event.findById(eventId);
+
+    // if event is completed, wont do anything
+    if (event.status === 'completed') {
+      res.status(400).json({ message: 'Event already completed' });
+    }
+
+    // get the point based on the time difference between start and end time
+    let period = event.end.getTime() - event.start.getTime();
+    period = period / 1000 / 60 / 60; // convert from milisecond to hours
+    let point = period * 10;
+
+    // check if the event is already overdue and deduct point accordingly
+    const currentTime = new Date();
+    if (event.end.getTime() < currentTime.getTime()) {
+      point = Math.floor(point / 2);
+    }
+
+    // Find EventParticipation of the users with that event ID and status == accepted,
+    // then for each Event Participation, update the point of that user
+
+    const participation = await EventParticipation.find({
+      eventId: eventId,
+      status: 'accepted',
+    });
+
+    for (let i = 0; i < participation.length; i++) {
+      await User.findByIdAndUpdate(
+        { _id: participation[i].userId },
+        { $inc: { points: point } },
+        { new: true },
+      );
+    }
+    await Event.findByIdAndUpdate(
+      { _id: eventId },
+      { status: 'completed' },
+      { new: true },
+    );
+
+    res.status(200).json({ event: event });
+  } catch (error) {
+    console.log(`Failed to modify event: ${error}`);
+    res.status(500).json({ error: error });
+  }
+};
+
+/**
+ * Check for overdued event and change status accordingly
+ * Asignee: Dang
+ * Name: finishEvent
+ * req: request object containing event id.
+ * res: response object to return status 200.
+ */
+const checkIfEventOverdue = async (req, res) => {
+  try {
+    const eventId = req.params.id;
+    const timeNow = new Date();
+    const event = await Event.findById(eventId);
+    if (event.status === 'completed' || event.status === 'overdue') {
+      res.status(200).json('Event is already completed or overdue');
+    } else {
+      if (event.end.getTime() < timeNow.getTime()) {
+        await Event.findByIdAndUpdate(
+          { _id: eventId },
+          { $set: { status: 'overdue' } },
+          { new: true },
+        );
+        res.status(200).json('Event is overdue');
+      }
+    }
+  } catch (error) {
+    console.log(`Failed to modify event: ${error}`);
+    res.status(500).json({ error: error });
+  }
+};
 
 module.exports = {
   createEvent,
+  modifyEvent,
+  finishEvent,
 };
