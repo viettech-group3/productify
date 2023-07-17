47c61165aa8ccab3f657c717d81ad81e14e8f171
deidaraiorek <100989552+deidaraiorek@users.noreply.github.com>
2023-07-17 00:33:47 -0400

    Added a funny feature (#53)
    * Added a funny feature
    
    * fix sutdy room bug
    
    * Added real forget pass
    
    * Fix styling
    
    ---------
    
    Co-authored-by: Jenny Quach <65835856+hoatuyet423@users.noreply.github.com>

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -7,19 +7,24 @@ const {
   leaderboard,
   getUser,
   updateUser,
+  forgotPassword,
+  resetPassword,
   getLabelList,
   addLabelList,
-  deleteLabelList
+  deleteLabelList,
 } = require('../controllers/User');
 const { protect } = require('../utils/auth');
+const User = require('../models/User');
 
 // Set up routes
 UserRouter.post('/', signUp);
+UserRouter.post('/forgotpassword', forgotPassword);
+UserRouter.post('/resetpassword', resetPassword);
 UserRouter.post('/login', login);
 UserRouter.get('/leaderboard', leaderboard);
 UserRouter.get('/getUser', protect, getUser);
 UserRouter.put('/update', protect, updateUser);
-UserRouter.get('/getLabelList', protect, getLabelList)
-UserRouter.post('/addLabelList', protect, addLabelList)
-UserRouter.delete('/deleteLabelList', protect, deleteLabelList)
+UserRouter.get('/getLabelList', protect, getLabelList);
+UserRouter.post('/addLabelList', protect, addLabelList);
+UserRouter.delete('/deleteLabelList', protect, deleteLabelList);
 module.exports = UserRouter;
--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -10,19 +10,25 @@ import {
   Tabs,
   Flex,
   Center,
+  Button,
   Image,
 } from '@chakra-ui/react';
 import Login from '../components/Authentication/Login';
 import Signup from '../components/Authentication/Signup';
 import logo from '../assets/images/logoWhite.png';
 import toast, { Toaster } from 'react-hot-toast';
-
+import styles from './AuthPage.module.css';
+import catMeme from '../assets/images/CatMeme.jpg';
+import axios from 'axios';
 const AuthPage = () => {
   if (localStorage.getItem('user')) {
     //If we already login, it will redirect to Home Page
     window.location.href = '/calendar';
   }
   const [activeTab, setActiveTab] = useState(0); //acTive Tab to control the <TabPanel> that is displayed in <TabPanels>
+  const [showModal, setShowModal] = useState(false); // State for showing/hiding the modal
+  const [email, setEmail] = useState('');
+
   const handleLoginClick0 = () => {
     //If we click the Login => Change to Login Tab
     setActiveTab(0);
@@ -38,6 +44,43 @@ const AuthPage = () => {
     setActiveTab(index);
   };
 
+  const openModal = () => {
+    // Function to open the modal
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    // Function to close the modal
+    setShowModal(false);
+  };
+
+  const handleResetPassword = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:5000/api/users/forgotpassword',
+        { email },
+      );
+
+      if (response.status === 200) {
+        // console.log(response.data);
+        // const { token } = response.data;
+        // window.location.href = `/forgotpassword?email=${encodeURIComponent(
+        //   email,
+        // )}&token=${encodeURIComponent(token)}`;
+        toast.success('Reset password email sent');
+        setShowModal(false);
+      } else {
+        toast.error('Email not found');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(' Can not change password right now');
+    }
+  };
+  const handleEmailChange = e => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
   return (
     <div>
       <Flex
@@ -96,12 +139,14 @@ const AuthPage = () => {
                     'linear-gradient(315deg, #fde7f9 0%, #aacaef 74%)',
                 }}
                 width="50%"
+                className={styles.tab}
               >
                 Login
               </Tab>
               <Tab
                 width="50%"
                 bg="white"
+                className={styles.tab}
                 _selected={{
                   backgroundImage:
                     'linear-gradient(315deg, #fde7f9 0%, #aacaef 74%)',
@@ -113,16 +158,25 @@ const AuthPage = () => {
             <TabPanels>
               <TabPanel>
                 <Login />
-                <div className="mt-3 text-center">
-                  <b>
-                    Don't Have Account?{' '}
-                    <span
-                      style={{ color: '#0077b6' }}
-                      onClick={handleLoginClick1}
-                    >
-                      Sign Up
-                    </span>
-                  </b>{' '}
+                <div className="mt-3 d-flex justify-content-between">
+                  <div>
+                    <b>
+                      Don't Have Account?{' '}
+                      <span
+                        style={{ color: '#0077b6' }}
+                        onClick={handleLoginClick1}
+                      >
+                        Sign Up
+                      </span>
+                    </b>
+                  </div>
+                  <div>
+                    <b>
+                      <span className={styles.forgetpass} onClick={openModal}>
+                        Forget your password ?
+                      </span>{' '}
+                    </b>
+                  </div>
                   {/* If we click Sign Up, it will move to Sign Up Components */}
                 </div>
               </TabPanel>
@@ -147,6 +201,94 @@ const AuthPage = () => {
           </Tabs>
         </Box>
       </Flex>
+      {showModal && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 50,
+          }}
+        >
+          <Box
+            bg="white"
+            p={4}
+            borderRadius="md"
+            maxWidth="500px"
+            position="relative"
+          >
+            <Box borderBottom="1px solid #000">
+              <Text fontSize="lg" fontWeight="bold">
+                Forget your password?
+              </Text>
+            </Box>
+            <Box py={4}>
+              <Text>Relax and try to remember your password, my friend.</Text>
+              <Text>(I know you can do it!!!)</Text>
+            </Box>
+            <Flex justifyContent="center" alignItems="center" height="200px">
+              <Image
+                src={catMeme}
+                alt="Meme"
+                boxSize="200px"
+                objectFit="cover"
+              />
+            </Flex>
+            <Flex flexDirection="column" alignItems="center" py={4}>
+              <p style={{ fontSize: 'x-small', margin: '0' }}>
+                In case you gave up, type your email and change password here:
+              </p>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <input
+                  type="email"
+                  onChange={handleEmailChange}
+                  style={{
+                    padding: '6px',
+                    borderRadius: '4px',
+                    border: '1px solid #ccc',
+                    outline: 'none',
+                    fontSize: 'x-small',
+                  }}
+                />
+                <button
+                  onClick={handleResetPassword}
+                  style={{
+                    padding: '4px 8px',
+                    borderRadius: '4px',
+                    border: 'none',
+                    backgroundColor: '#337ab7',
+                    color: '#fff',
+                    fontSize: 'x-small',
+                    cursor: 'pointer',
+                    height: '26px',
+                    marginLeft: '8px',
+                  }}
+                >
+                  Submit
+                </button>
+              </div>
+            </Flex>
+
+            <Box pt={1}>
+              <Flex justifyContent="flex-end">
+                <Button
+                  colorScheme="blue"
+                  variant="outline"
+                  onClick={closeModal}
+                >
+                  Thanks
+                </Button>
+              </Flex>
+            </Box>
+          </Box>
+        </div>
+      )}
       <Toaster />
     </div>
   );
